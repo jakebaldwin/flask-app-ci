e7e8a80261d708bec667fc2ae05a4c5107c4972e
Jake Baldwin <jakebaldwin17@gmail.com>
2020-12-30 13:01:47 -0500

    playing around locally with deployment

---
+++ jake-flask-app.py
@@ -0,0 +1,96 @@
+import os
+
+import boto3
+
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+USERS_TABLE = os.environ['USERS_TABLE']
+IS_OFFLINE = os.environ.get('IS_OFFLINE')
+
+if IS_OFFLINE:
+    client = boto3.client(
+        'dynamodb',
+        region_name='localhost',
+        endpoint_url='http://localhost:8000'
+    )
+else:
+    client = boto3.client('dynamodb')
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/users/<string:user_id>")
+def get_user(user_id):
+    resp = client.get_item(
+        TableName=USERS_TABLE,
+        Key={
+            'userId': {'S': user_id}
+        }
+    )
+    item = resp.get("Item")
+    if not item:
+        return jsonify({'error': 'User does not exist'}), 404
+
+    return jsonify({
+        'userId': item.get("userId").get('S'),
+        'name': item.get("name").get('S')
+    })
+
+
+@app.route("/users", methods=["POST"])
+def create_user():
+    user_id = request.json.get("userId")
+    name = request.json.get("name")
+    if not user_id or not name:
+        return jsonify({"error": "Please provide userId and name"}), 400
+
+    resp = client.put_item(
+        TableName=USERS_TABLE,
+        Item={
+            'userId': {'S': user_id},
+            'name': {'S': name}
+        }
+    )
+
+    return jsonify({
+        "userId": user_id,
+        "name": name
+    })
+
+
+@app.route("/users", methods=["PUT"])
+def update_user():
+    user_id = request.json.get("userId")
+    named = request.json.get("name")
+    resp = client.update_item(
+        TableName=USERS_TABLE,
+        Key={
+            'userId': {'S': user_id}
+        },
+        UpdateExpression="set #nm=:r",
+        ExpressionAttributeValues={
+            ':r': {'S': named}
+        },
+        ExpressionAttributeNames={
+            "#nm": "name"
+        }
+    )
+    return get_user(user_id)
+
+
+@app.route("/users", methods=["DELETE"])
+def delete_user():
+    user_id = request.json.get("userId")
+    if get_user(user_id):
+        resp = client.delete_item(
+            TableName=USERS_TABLE,
+            Key={
+                'userId': {'S': user_id}
+            }
+        )
+        return resp
+    return {'error': 'User does not exist'}
